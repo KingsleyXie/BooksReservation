8881a7ae2bc481eeed79d5cc166ce72efa05db87
Kingsley <kingsley.xym@gmail.com>
2017-08-16 20:57:55 +0800

    Format Front-End Code of Login Module

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,19 +1,17 @@
 $(document).ready(function() {
-	$('#loginform').submit(function(e) {
+	$('#login-form').submit(function(e) {
 		e.preventDefault();
 		$.ajax({
-			type: "POST",
-			url: "../assets/API/admin.php",
-			data: $(this).serialize() + "&type=" + 1,
+			type: 'POST',
+			url: '../assets/API/admin.php',
+			data: $(this).serialize() + '&type=' + 1,
 			success: function(response)
 			{
-				if (response.code == 1)
-					alert("用户名或密码错误！");
-				if (response.code == 2)
-					alert("请输入用户名和密码！");
 				if (response.code == 0) {
-					alert("登录成功！");
-					window.location.href = "./";
+					alert('登录成功！');
+					window.location.href = './';
+				} else {
+					alert(response.errMsg);
 				}
 			}
 		});
