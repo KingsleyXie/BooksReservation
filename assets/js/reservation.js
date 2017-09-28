bec8b30261403e8d072da7629ae3bfc08fde1f96
Kingsley <kingsley.xym@gmail.com>
2017-09-28 13:32:59 +0800

    Reorganize Reservation Module

--- assets/js/reservation.js
+++ assets/js/reservation.js
@@ -13,76 +13,74 @@ $(document).ready(function() {
 				window.location.href = './login';
 			};
 			if (response[0].code == 0) {
-				for (var res = 1; res < response.length; res++) {
-					document.getElementById("reservation").innerHTML += 
-					'<div class="card">' + 
-						'<div class="card-content">' + 
-							'<div class="card-title">订单号：' + response[res].reservationNo + '</div>' + 
-							'<div class="card-details">' + 
-								'<table class="highlight responsive-table">' + 
-									'<thead>' + 
-										'<tr>' + 
-											'<th>姓名</th>' + 
-											'<th>学号</th>' + 
-											'<th>联系方式</th>' + 
-											'<th>宿舍楼</th>' + 
-											'<th>领取日期</th>' + 
-											'<th>领取时段</th>' + 
-											'<th>订单提交时间</th>' + 
-											'<th>订单更新时间</th>' + 
-										'</tr>' + 
-									'</thead>' + 
-									'<tbody>' + 
-										'<tr>' + 
-											'<td>' + response[res].stuName + '</td>' + 
-											'<td>' + response[res].stuNo + '</td>' + 
-											'<td>' + response[res].contact + '</td>' + 
-											'<td>' + response[res].dormitory + '</td>' + 
-											'<td>' + response[res].date + '</td>' + 
-											'<td>' + response[res].timePeriod + '</td>' + 
-											'<td>' + response[res].sbmTime + '</td>' + 
-											'<td>' + response[res].updTime + '</td>' + 
-										'</tr>' + 
-									'</tbody>' + 
-								'</table>' + 
-								'<div class="section">' + 
-									'<div class="card-title">预约书籍信息：</div>' + 
-									'<div class="row" id="reservedBooksFor' + response[res].reservationNo + '"></div>' + 
-								'</div>' + 
-							'</div>' + 
-						'</div>';
-					'</div>';
+				for (var index = 1; index < response.length; index++) {
+					$("#reservation").append(
+					'<div class="card">' +
+						'<div class="card-content">' +
+							'<div class="card-title">订单号：' + response[index].reservationNo + '</div>' +
+							'<div class="card-details">' +
+								'<table class="highlight responsive-table">' +
+									'<thead>' +
+										'<tr>' +
+											'<th>姓名</th>' +
+											'<th>学号</th>' +
+											'<th>联系方式</th>' +
+											'<th>宿舍楼</th>' +
+											'<th>领取日期</th>' +
+											'<th>领取时段</th>' +
+											'<th>订单提交时间</th>' +
+											'<th>订单更新时间</th>' +
+										'</tr>' +
+									'</thead>' +
+									'<tbody>' +
+										'<tr>' +
+											'<td>' + response[index].stuName + '</td>' +
+											'<td>' + response[index].stuNo + '</td>' +
+											'<td>' + response[index].contact + '</td>' +
+											'<td>' + response[index].dormitory + '</td>' +
+											'<td>' + response[index].date + '</td>' +
+											'<td>' + response[index].timePeriod + '</td>' +
+											'<td>' + response[index].sbmTime + '</td>' +
+											'<td>' + response[index].updTime + '</td>' +
+										'</tr>' +
+									'</tbody>' +
+								'</table>' +
+								'<div class="section">' +
+									'<div class="card-title">预约书籍信息：</div>' +
+									'<div class="row" id="reserved-books-for' + response[index].reservationNo + '"></div>' +
+								'</div>' +
+							'</div>' +
+						'</div>' +
+					'</div>');
 
-					for (var i = 0; i < response[res].books.length; i++) {
-
-						var MultipleAuthor = response[res].books[i].isMultipleAuthor == 1 ? ' 等' : '';
-
-						if (response[res].books[i].image == './assets/pictures/defaultCover.png') {
-							response[res].books[i].image = '../assets/pictures/defaultCover.png'
+					$.each(response[index].books, function(i, book) {
+						var MultipleAuthor = book.isMultipleAuthor == 1 ? ' 等' : '';
+						if (book.image == './assets/pictures/defaultCover.png') {
+								book.image = '../assets/pictures/defaultCover.png'
 						}
 
-						document.getElementById('reservedBooksFor' + response[res].reservationNo).innerHTML += 
-							'<div class="col s12 m4">' + 
-								'<div class="card blue-grey darken-1">' + 
-									'<div class="card-content white-text">' + 
-										'<div class="card-title">#' + response[res].books[i].bookID + ' ' + response[res].books[i].title + 
-										'</div>' + 
-										'<div class="card-details">' + 
-											'<p>作者：' + response[res].books[i].author + MultipleAuthor + '</p>' + 
-											'<p>出版社：' + response[res].books[i].press + '</p>' + 
-											'<p>出版日期：' + response[res].books[i].pubdate + '</p>' + 
-										'</div>' + 
-									'</div>' + 
-									'<div class="card-action center-align">' + 
-										'<a class="cover-reservation" href="' + response[res].books[i].image + '">触碰或点击查看封面图片' + 
-											'<div class="cover-image-reservation">' + 
-												'<img src="' + response[res].books[i].image + '" alt="封面图片">' + 
-											'</div>' + 
-										'</a>' + 
-									'</div>' + 
-								'</div>' + 
-							'</div>';
-					}
+						$("#reserved-books-for" + response[index].reservationNo).append(
+						'<div class="col s12 m4">' +
+							'<div class="card blue-grey darken-1">' +
+								'<div class="card-content white-text">' +
+									'<div class="card-title">#' + book.bookID + ' ' + book.title +
+									'</div>' +
+									'<div class="card-details">' +
+										'<p>作者：' + book.author + MultipleAuthor + '</p>' +
+										'<p>出版社：' + book.press + '</p>' +
+										'<p>出版日期：' + book.pubdate + '</p>' +
+									'</div>' +
+								'</div>' +
+								'<div class="card-action center-align">' +
+									'<a class="cover-reservation" href="' + book.image + '">触碰或点击查看封面图片' +
+										'<div class="cover-image-reservation">' +
+											'<img src="' + book.image + '" alt="封面图片">' +
+										'</div>' +
+									'</a>' +
+								'</div>' +
+							'</div>' +
+						'</div>');
+					});
 					$("#loading").hide();
 				}
 			}
