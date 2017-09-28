996904235472688cbe46499f17160896a25f643e
Kingsley <kingsley.xym@gmail.com>
2017-09-29 00:41:04 +0800

    Optimize Back-End Config File

--- assets/API/config.php
+++ assets/API/config.php
@@ -1,4 +1,5 @@
 <?php
+/* --- Configuration Part Start --- */
 
 //Database Configurations:
 $addr = 'localhost';	//Database Address
@@ -6,38 +7,37 @@ $dbname = 'books_reservation';	//Database Name
 $user = 'books_reservation_DB_username_here';	//Username for Project Database
 $password = 'corresponding_password_here';	//Password for Project Database
 
+/* --- Configuration Part End --- */
+
+
+
+
+
 //Database Connection based on PDO:
 try {
 	$connect = new PDO("mysql:host=$addr;dbname=$dbname;charset=utf8", $user, $password);
 } catch(PDOException $ex) {
     response(2333, '数据库连接出错，请联系管理员');
-    exit(0);
 }
-
-
 
 //Return Code Process Function:
 function response($code, $errMsg='success') {
-	echo json_encode(array('code' => $code, 'errMsg' => $errMsg));
+	echo json_encode(['code' => $code, 'errMsg' => $errMsg]);
 	exit(0);
 }
 
 //Check whether required paraments exist or not
 function existCheck() {
 	for($i = 0; $i < func_num_args(); $i++) {
-		if (!isset($_POST[func_get_arg($i)])) {
+		if (!isset($_POST[func_get_arg($i)]))
 			header('Location: http://p1.img.cctvpic.com/20120409/images/1333902721891_1333902721891_r.jpg');
-			exit(0);
-		}
 	}
 }
 
 //Check if necessary paraments are blank
 function blankCheck() {
 	for($i = 0; $i < func_num_args(); $i++) {
-		if (($_POST[func_get_arg($i)] == '') OR ($_POST[func_get_arg($i)] === 0)) {
+		if (($_POST[func_get_arg($i)] == '') OR ($_POST[func_get_arg($i)] === 0))
 			response(233, '必填项中含有空值');
-			exit(0);
-		}
 	}
 }
