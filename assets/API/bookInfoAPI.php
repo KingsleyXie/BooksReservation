b228f7cd0194fe5cb4ff04aa2440eb197cd961fa
Kingsley <kingsley.xym@gmail.com>
2017-09-29 00:55:08 +0800

    Optimize Code Of ISBN API

--- assets/API/bookInfoAPI.php
+++ assets/API/bookInfoAPI.php
@@ -1,17 +1,23 @@
 <?php
+//Prevent Error Report In Case The Returned Data Is Not Complete
+error_reporting(0);
 header('Content-Type: application/json');
 require_once('./config.php');
+
+existCheck('ISBN');
+blankCheck('ISBN');
 
 $handle = @fopen("https://api.douban.com/v2/book/isbn/" . $_POST['ISBN'],"rb");
 if (empty($_POST['ISBN']) OR !$handle) {
 	response(1, '未找到书籍信息，请手动录入相关数据');
-}
-else {
+} else {
 	$data = "";
-	while (!feof($handle)) {
+	while (!feof($handle))
 		$data .= fread($handle, 10000);
-	}
 	fclose($handle);
 	$prep = json_decode($data);
-	echo json_encode(array('code' => 0, 'title' => $prep->title, 'author' => $prep->author[0], 'isMultipleAuthor' => count($prep->author)==1 ? 0:1, 'press' => $prep->publisher, 'pubdate' => $prep->pubdate, 'image' => $prep->images->large));
+	echo json_encode(['code' => 0, 'title' => $prep->title, 'author' => $prep->author[0], 'isMultipleAuthor' => count($prep->author)==1 ? 0 : 1, 'press' => $prep->publisher, 'pubdate' => $prep->pubdate, 'image' => $prep->images->large]);
+	exit(0);
 }
+
+response(2, '出现未知错误，请联系管理员');
