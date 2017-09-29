429ee6b59e569e8404fbfba98a5cc00672357d33
Kingsley <kingsley.xym@gmail.com>
2017-09-29 14:19:45 +0800

    Reorganize Back-End Code Of Reserve Module

--- assets/API/modify.php
+++ assets/API/modify.php
@@ -2,72 +2,132 @@
 header('Content-Type: application/json');
 require_once('./config.php');
 
-if (!isset($_POST['count'])) {
-	header('Location: http://p1.img.cctvpic.com/20120409/images/1333902721891_1333902721891_r.jpg');
-	return;
-}
+existCheck('count', 'list0', 'list1', 'list2', 'preList0', 'preList1', 'preList2');
+blankCheck('studentName', 'studentNo', 'dormitory', 'contact', 'date', 'timePeriod');
 
-if ($_POST['count'] <= 0 OR empty($_POST['studentName']) OR empty($_POST['studentNo']) OR empty($_POST['dormitory']) OR empty($_POST['contact']) OR empty($_POST['date']) OR empty($_POST['timePeriod']) OR !isset($_POST['list0']) OR !isset($_POST['list1']) OR !isset($_POST['list2'])) 
-	response(1, '请将预约信息填写完整！');
+//Duplication Check
+if (
+	($_POST['count'] <= 0 OR $_POST['count'] > 3)
+	OR (
+		($_POST['count'] == 2)
+		AND ($_POST['list0'] == $_POST['list1'])
+	)
+	OR (
+		($_POST['count'] == 3)
+		AND (
+			($_POST['list0'] == $_POST['list1'])
+			OR ($_POST['list1'] == $_POST['list2'])
+			OR ($_POST['list0'] == $_POST['list2'])
+		)
+	)
+) response(3, '错误请求');
 
-$dup = 0;
-if ($_POST['count'] == 2 && $_POST['list0'] == $_POST['list1']) {
-	$dup = 1;
-}
-if ($_POST['count'] == 3 && (($_POST['list0'] == $_POST['list1']) OR ($_POST['list1'] == $_POST['list2']) OR ($_POST['list0'] == $_POST['list2']))) {
-	$dup = 1;
-}
-
-if ($_POST['count'] > 3 OR $dup == 1) response(2, '错误请求');
-
-
-
-
-$sql = 'SELECT * FROM books WHERE (bookID = ? AND remainingAmount <= 0) OR (bookID = ? AND remainingAmount <= 0) OR (bookID = ? AND remainingAmount <= 0)';
+$sql = '
+SELECT * FROM books
+WHERE
+	(bookID = ? AND remainingAmount <= 0)
+	OR (bookID = ? AND remainingAmount <= 0)
+	OR (bookID = ? AND remainingAmount <= 0)';
 $stmt = $connect->prepare($sql);
-$stmt->execute(array($_POST['list0'], $_POST['list1'], $_POST['list2']));
+$stmt->execute([
+	$_POST['list0'],
+	$_POST['list1'],
+	$_POST['list2']
+]);
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 if (!empty($result)) {
 	date_default_timezone_set('Asia/Shanghai');
 	$time=date('Y.m.d H:i:s');
-	$logFile = fopen("../collision.log", "a");
-	$logText = "[" . $time . "]\tstudentNo:" . $_POST['studentNo'] . "\tlist0:" . $_POST['list0'] . "\tlist1:" . $_POST['list1'] . "\tlist2:" . $_POST['list2'] . "\n\n";
+	$logFile = fopen('../collision.log', 'a');
+	$logText =
+		"[" . $time . "]\tstudentNo:" .
+		$_POST['studentNo'] .
+		"\tlist0:" . $_POST['list0'] .
+		"\tlist1:" . $_POST['list1'] .
+		"\tlist2:" . $_POST['list2'] .
+		"\n\n";
 	fwrite($logFile, $logText);
 	fclose($logFile);
 
 	response(3, '列表中有书籍已被他人预约，请重新选择<br><br>预约信息不需要重新填写O(∩_∩)O');
 }
 
-$sql = 'UPDATE `students` SET `studentName`=?, `dormitory`=?, `contact`=? WHERE studentNo=?';
+$sql = '
+UPDATE `students`
+SET
+	`studentName` = ?,
+	`dormitory` = ?,
+	`contact` = ?
+WHERE
+	studentNo = ?';
 $stmt = $connect->prepare($sql);
-$stmt->execute(array($_POST['studentName'], $_POST['dormitory'], $_POST['contact'], $_POST['studentNo']));
+$stmt->execute([
+	$_POST['studentName'],
+	$_POST['dormitory'],
+	$_POST['contact'],
+	$_POST['studentNo']
+]);
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 if (empty($result)) {
-	$sql = 'SELECT studentID from students WHERE studentNo = ?';
+	$sql = 'SELECT studentID FROM students WHERE studentNo = ?';
 	$stmt = $connect->prepare($sql);
-	$stmt->execute(array($_POST['studentNo']));
+	$stmt->execute([$_POST['studentNo']]);
 	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	$stuID = $result[0]['studentID'];
 
-	$sql = 'UPDATE `reservations` SET `date`=?, `timePeriod`=?, `bookID_1`=?, `bookID_2`=?, `bookID_3`=? WHERE studentID=?';
+	$sql = '
+	UPDATE `reservations`
+	SET
+		`date` = ?,
+		`timePeriod` = ?,
+		`bookID_1` = ?,
+		`bookID_2` = ?,
+		`bookID_3` = ? 
+	WHERE studentID = ?';
 	$stmt = $connect->prepare($sql);
-	$stmt->execute(array($_POST['date'], $_POST['timePeriod'], $_POST['list0'], $_POST['list1'], $_POST['list2'], $stuID));
+	$stmt->execute([
+		$_POST['date'],
+		$_POST['timePeriod'],
+		$_POST['list0'],
+		$_POST['list1'],
+		$_POST['list2'],
+		$stuID
+	]);
 	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 	if (empty($result)) {
-		$sql = 'UPDATE books SET remainingAmount = remainingAmount + 1 WHERE (bookID = ?) OR (bookID = ?) OR (bookID = ?)';
+		$sql = '
+		UPDATE books
+		SET remainingAmount = remainingAmount + 1
+		WHERE
+			(bookID = ?)
+			OR (bookID = ?)
+			OR (bookID = ?)';
 		$stmt = $connect->prepare($sql);
-		$stmt->execute(array($_POST['preList0'], $_POST['preList1'], $_POST['preList2']));
+		$stmt->execute([
+			$_POST['preList0'],
+			$_POST['preList1'],
+			$_POST['preList2']
+		]);
 		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-		$sql = 'UPDATE books SET remainingAmount = remainingAmount - 1 WHERE (bookID = ?) OR (bookID = ?) OR (bookID = ?)';
+		$sql = '
+		UPDATE books
+		SET remainingAmount = remainingAmount - 1
+		WHERE
+			(bookID = ?)
+			OR (bookID = ?)
+			OR (bookID = ?)';
 		$stmt = $connect->prepare($sql);
-		$stmt->execute(array($_POST['list0'], $_POST['list1'], $_POST['list2']));
+		$stmt->execute([
+			$_POST['list0'],
+			$_POST['list1'],
+			$_POST['list2']
+		]);
 		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		if (empty($result)) {
+		if (empty($result))
 			response(0);
-		}
 	}
 }
 
--- assets/API/reserve.php
+++ assets/API/reserve.php
@@ -2,72 +2,115 @@
 header('Content-Type: application/json');
 require_once('./config.php');
 
-if (!isset($_POST['count'])) {
-	header('Location: http://p1.img.cctvpic.com/20120409/images/1333902721891_1333902721891_r.jpg');
-	return;
-}
+existCheck('count', 'list0', 'list1', 'list2');
+blankCheck('studentName', 'studentNo', 'dormitory', 'contact', 'date', 'timePeriod');
 
-if ($_POST['count'] <= 0 OR empty($_POST['studentName']) OR empty($_POST['studentNo']) OR empty($_POST['dormitory']) OR empty($_POST['contact']) OR empty($_POST['date']) OR empty($_POST['timePeriod']) OR !isset($_POST['list0']) OR !isset($_POST['list1']) OR !isset($_POST['list2'])) {
-	response(1, '请将预约信息填写完整！');
-}
+//Duplication Check
+if (
+	($_POST['count'] <= 0 OR $_POST['count'] > 3)
+	OR (
+		($_POST['count'] == 2)
+		AND ($_POST['list0'] == $_POST['list1'])
+	)
+	OR (
+		($_POST['count'] == 3)
+		AND (
+			($_POST['list0'] == $_POST['list1'])
+			OR ($_POST['list1'] == $_POST['list2'])
+			OR ($_POST['list0'] == $_POST['list2'])
+		)
+	)
+) response(3, '错误请求');
 
-$dup = 0;
-if ($_POST['count'] == 2 && $_POST['list0'] == $_POST['list1']) {
-	$dup = 1;
-}
-if ($_POST['count'] == 3 && (($_POST['list0'] == $_POST['list1']) OR ($_POST['list1'] == $_POST['list2']) OR ($_POST['list0'] == $_POST['list2']))) {
-	$dup = 1;
-}
-
-if ($_POST['count'] > 3 OR $dup == 1) response(3, '错误请求');
-
-
-
-$sql = 'SELECT * from students WHERE studentNo = ?';
+$sql = 'SELECT * FROM students WHERE studentNo = ?';
 $stmt = $connect->prepare($sql);
-$stmt->execute(array($_POST['studentNo']));
+$stmt->execute([$_POST['studentNo']]);
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 if (!empty($result)) response(2, '该学号已存在预约订单！');
 
-
-$sql = 'SELECT * FROM books WHERE (bookID = ? AND remainingAmount <= 0) OR (bookID = ? AND remainingAmount <= 0) OR (bookID = ? AND remainingAmount <= 0)';
+$sql = '
+SELECT * FROM books
+WHERE
+	(bookID = ? AND remainingAmount <= 0)
+	OR (bookID = ? AND remainingAmount <= 0)
+	OR (bookID = ? AND remainingAmount <= 0)';
 $stmt = $connect->prepare($sql);
-$stmt->execute(array($_POST['list0'], $_POST['list1'], $_POST['list2']));
+$stmt->execute([
+	$_POST['list0'],
+	$_POST['list1'],
+	$_POST['list2']
+]);
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 if (!empty($result)) {
 	date_default_timezone_set('Asia/Shanghai');
 	$time=date('Y.m.d H:i:s');
-	$logFile = fopen("../collision.log", "a");
-	$logText = "[" . $time . "]\tstudentNo:" . $_POST['studentNo'] . "\tlist0:" . $_POST['list0'] . "\tlist1:" . $_POST['list1'] . "\tlist2:" . $_POST['list2'] . "\n\n";
+	$logFile = fopen('../collision.log', 'a');
+	$logText =
+		"[" . $time . "]\tstudentNo:" .
+		$_POST['studentNo'] .
+		"\tlist0:" . $_POST['list0'] .
+		"\tlist1:" . $_POST['list1'] .
+		"\tlist2:" . $_POST['list2'] .
+		"\n\n";
 	fwrite($logFile, $logText);
 	fclose($logFile);
 
 	response(5, '列表中有书籍已被他人预约，请重新选择<br><br>预约信息不需要重新填写O(∩_∩)O');
 }
 
-
-
-$sql = 'INSERT INTO `students`(`studentName`, `studentNo`, `dormitory`, `contact`)  VALUES (?,?,?,?)';
+$sql = '
+INSERT INTO `students`
+	(`studentName`, `studentNo`, `dormitory`, `contact`)
+VALUES
+	(?,?,?,?)';
 $stmt = $connect->prepare($sql);
-$stmt->execute(array($_POST['studentName'], $_POST['studentNo'], $_POST['dormitory'], $_POST['contact']));
+$stmt->execute([
+	$_POST['studentName'],
+	$_POST['studentNo'],
+	$_POST['dormitory'],
+	$_POST['contact']
+]);
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 if (empty($result)) {
-	$sql = 'SELECT studentID from students WHERE studentNo = ?';
+	$sql = 'SELECT studentID FROM students WHERE studentNo = ?';
 	$stmt = $connect->prepare($sql);
-	$stmt->execute(array($_POST['studentNo']));
+	$stmt->execute([$_POST['studentNo']]);
 	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	$stuID = $result[0]['studentID'];
 
-	$sql = 'INSERT INTO `reservations`(`date`, `timePeriod`, `studentID`, `bookID_1`, `bookID_2`, `bookID_3`) VALUES (?,?,?,?,?,?)';
+	$sql = '
+	INSERT INTO `reservations`
+		(`date`, `timePeriod`, `studentID`, `bookID_1`, `bookID_2`, `bookID_3`)
+	VALUES
+		(?,?,?,?,?,?)';
 	$stmt = $connect->prepare($sql);
-	$stmt->execute(array($_POST['date'], $_POST['timePeriod'], $stuID, $_POST['list0'], $_POST['list1'], $_POST['list2']));
+	$stmt->execute([
+		$_POST['date'],
+		$_POST['timePeriod'],
+		$stuID,
+		$_POST['list0'],
+		$_POST['list1'],
+		$_POST['list2']
+	]);
 	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 	if (empty($result)) {
-		$sql = 'UPDATE books SET remainingAmount = remainingAmount - 1 WHERE (bookID = ?) OR (bookID = ?) OR (bookID = ?)';
+		$sql = '
+		UPDATE books
+		SET
+			remainingAmount = remainingAmount - 1
+		WHERE
+			(bookID = ?)
+			OR (bookID = ?)
+			OR (bookID = ?)';
 		$stmt = $connect->prepare($sql);
-		$stmt->execute(array($_POST['list0'], $_POST['list1'], $_POST['list2']));
+		$stmt->execute([
+			$_POST['list0'],
+			$_POST['list1'],
+			$_POST['list2']
+		]);
 		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		if (empty($result)) response(0);
 	}
