085d432b5b981ff8281c1826688a6a4c1e043f25
Kingsley <kingsley.xym@gmail.com>
2018-05-01 22:55:32 +0800

    :sparkles: Desgin API Routes

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,59 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Admin Part
+Route::get(
+	'/api/admin/login',
+	'AdminController@login'
+);
+Route::get(
+	'/api/admin/logout',
+	'AdminController@logout'
+);
 
 Route::get(
-	'/test',
-	'TestController@index'
+	'/api/admin/book/all',
+	'AdminController@getAllBooks'
+);
+Route::post(
+	'/api/admin/book/add',
+	'AdminController@addBook'
+);
+Route::post(
+	'/api/admin/book/update/{id}',
+	'AdminController@updateBookById'
+);
+
+Route::get(
+	'/api/admin/reservation/all',
+	'ReservationController@index'
+);
+
+Route::get(
+	'/api/admin/douban/search/isbn/{isbn}',
+	'DoubanController@search'
+);
+
+// User Part
+Route::get(
+	'/api/user/reservation/stuno/{stuno}',
+	'ReservationController@searchByStuNo'
+);
+
+Route::post(
+	'/api/user/reserve/add'
+	'ReserveController@add'
+);
+Route::post(
+	'/api/user/reserve/modify/{id}',
+	'ReserveController@modifyReservationById'
+);
+
+Route::get(
+	'/api/user/book/all',
+	'BookController@index'
+);
+Route::get(
+	'/api/user/book/search',
+	'BookController@search'
 );
