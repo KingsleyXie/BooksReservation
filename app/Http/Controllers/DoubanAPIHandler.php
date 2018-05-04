a52615e96ea3649090ab36c19afc465507c3312f
Kingsley <kingsley.xym@gmail.com>
2018-05-05 03:32:04 +0800

    :sparkles: Add Douban Image 403 Problem Handler

--- app/Http/Controllers/DoubanAPIHandler.php
+++ app/Http/Controllers/DoubanAPIHandler.php
@@ -20,13 +20,24 @@ class DoubanAPIHandler extends Controller
 		if (count($book['author']) > 1)
 			$author .= ' 等';
 
+		// Handle Douban image 403 forbidden problem
+		// with replacement of previous url
+		//
+		// Another solution: replace it with
+		// https://images.weserv.nl/?url=douban_image_path
+		$cover = preg_replace(
+			'/(.*?)\/view\/subject\/l\/public\/(.*)/',
+			'$1/lpic/$2',
+			$book['images']['large']
+		);
+
 		return [
 			'isbn' => $ISBN,
-			'title' => $book['title'],
 			'author' => $author,
+			'cover' => $cover,
+			'title' => $book['title'],
 			'publisher' => $book['publisher'],
-			'pubdate' => $book['pubdate'],
-			'cover' => $book['images']['large']
+			'pubdate' => $book['pubdate']
 		];
 	}
 }
